b3719a36c1b335e360970e626e6f7d31cce0660a
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-02-25 10:40:30 +0100

    Fixed language tags in the ontologies

--- dist/mvn-dm2e-validate.sh
+++ dist/mvn-dm2e-validate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -o -q exec:java -Dexec.mainClass=eu.dm2e.utils.Dm2eValidationCLI -Dexec.args="$*"
+mvn -q exec:java -Dexec.mainClass=eu.dm2e.utils.Dm2eValidationCLI -Dexec.args="$*"
