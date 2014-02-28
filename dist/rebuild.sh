4decbe9e8e6082dfeece11bc6d4fe24ca9d1ca46
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-03-01 00:32:44 +0100

    update rebuild.sh

--- dist/rebuild.sh
+++ dist/rebuild.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+echo "!!!!!!!"
+echo ""
+echo "Rebuilding should only be done on the server"
+echo ""
+echo "Are you sure you are on the server and don't have any changes staged for Dm2eValidationCLI.java?"
+echo ""
+echo "!!!!!!!"
+echo "(yes/no)"
+read yesno
+if [[ ! $yesno = "yes" ]];then
+    exit 2;
+fi
+echo "proceeding"
+
+
+echo "Reset CLI"
+git checkout -- src/main/java/eu/dm2e/utils/Dm2eValidationCLI.java
+
 echo "Pull from git"
 git pull
 
