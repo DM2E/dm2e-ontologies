a233f5d758d79d3ef5d5856dc486d116386a2ce6
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-04-11 14:20:36 +0200

    .

--- src/main/resources/quantitative_analysis/build_tables.py
+++ src/main/resources/quantitative_analysis/build_tables.py
@@ -1,5 +1,4 @@
 import csv
-import json
 import re
 import math
 import logging
@@ -11,26 +10,11 @@ def clean_uri(uri):
     return uri
 
 
-def first_number_column_index(arr):
-    i = 0
-    for item in arr:
-        try:
-            float(item)
-            break
-        except ValueError:
-            i += 1
-    return i
-
-
-def first_number_column(arr):
-    return arr[first_number_column(arr)]
-
-
 class NumberSeries:
 
     def __init__(self, key):
         logging.basicConfig(level=logging.DEBUG)
-        self.key = key
+        self.key = clean_uri(key)
         self.values = []
 
     def append(self, number):
@@ -42,28 +26,33 @@ class NumberSeries:
     def max(self):
         return max(self.values)
 
-    def length(self):
+    def size(self):
         return len(self.values)
 
     def sum(self):
         return sum(self.values)
 
-    def arithmean(self):
-        return self.sum() / float(len(self.values))
+    def amean(self):
+        return self.sum() / float(self.size())
 
-    def geomean(self):
+    def gmean(self):
         logsum = 0
         for val in self.values:
             logsum += math.log(val)
-        return logsum/self.length()
+        return math.exp(logsum/self.size())
+
+    def hmean(self):
+        return self.size() / sum(1. / val for val in self.values)
 
     def to_dict(self):
         return {
-            'key': self.key,
-            'max': self.max(),
-            'arithmean': self.arithmean(),
-            'geomean': self.geomean(),
-            'length': self.length()}
+            '?key': self.key,
+            '?max': self.max(),
+            '?min': self.min(),
+            '?amean': self.amean(),
+            '?gmean': self.gmean(),
+            '?hmean': self.hmean(),
+            '?size': self.size()}
 
 
 class TableBuilder:
@@ -77,6 +66,7 @@ class TableBuilder:
 
         self.per_dataset_stats = {}
         self.init_per_dataset_stats()
+        self.write_per_dataset_stats()
 
     def init_per_dataset_stats(self):
         with open(self.template_dir + 'numbers-per-dataset.tsv', 'rb') as csvin:
@@ -98,6 +88,8 @@ class TableBuilder:
             stats['?nr_stmt_poequal'] = self.count_lines(dataset, 'predicate-object-equal-statements')
             # ?nr_diff_host
             stats['?nr_diff_host'] = self.count_lines(dataset, 'hostnames')
+            # ?nr_diff_baseurl
+            stats['?nr_diff_baseurl'] = self.count_lines(dataset, 'hostnames')  # TODO
             # ?nr_diff_license
             stats['?nr_diff_license'] = self.count_lines(dataset, 'license')
             # ?nr_diff_types
@@ -108,6 +100,8 @@ class TableBuilder:
             stats['?nr_stmt_literal'] = self.get_second_line_as_number(dataset, 'literal-statements')
             # ?perc_stmt_literal
             stats['?perc_stmt_literal'] = 100 * stats['?nr_stmt_literal'] / float(stats['?nr_stmt_total'])
+            # ?perc_stmt_uri
+            stats['?perc_stmt_uri'] = 100 - stats['?perc_stmt_literal']
 
     def get_second_line_as_number(self, dataset, analysis):
         ret = 0
@@ -145,7 +139,7 @@ class TableBuilder:
         logging.debug("Average '%s'" % analysis)
         ret_tables = {}
 
-        # total_series = {}
+        total_series = {}
         for dataset in self.datasets:
             logging.debug("Average '%s' for dataset '%s'" % (analysis, dataset))
             dataset_series = {}
@@ -153,51 +147,28 @@ class TableBuilder:
                 reader = csv.DictReader(tsvin, delimiter='\t')
                 for row in reader:
                     if not row[key_group] in dataset_series.keys(): dataset_series[row[key_group]] = NumberSeries(row[key_group])
-                    # if not key_group in total_series.keys(): total_series[key_group] = NumberSeries(row[key_group])
+                    if not row[key_group] in total_series.keys(): total_series[row[key_group]] = NumberSeries(row[key_group])
                     dataset_series[row[key_group]].append(row[key_number])
-                    # total_series[key_group].append(row[key_number])
+                    total_series[row[key_group]].append(row[key_number])
                 ret_tables[dataset] = []
                 for series in dataset_series.itervalues(): ret_tables[dataset].append(series.to_dict())
-                break
-        # ret_tables['total'] = []
-        # for series in total_series.itervalues(): ret_tables['total'].append(series.to_dict())
-        return ret_tables
-
-    def visualize_by_number_of_lines(self):
-        print
-
-    def visualize_map(self, analysis):
-        headers = [['number', 'Lat'], ['number', 'Lon'], ['string', 'uri']]
-        aoa = []
-        with open(self.analyses_dir + analysis + '.rq.tsv', 'rb') as tsvin:
-            reader = csv.reader(tsvin, delimiter='\t')
-            first_line = True
-            for row_tsv in reader:
-                if len(row_tsv) < 3:
-                    logging.debug(row_tsv)
-                if first_line: first_line = False
-                else:
-                    this_row = []
-                    try:
-                        this_row.append(float(row_tsv[3]))
-                        this_row.append(float(row_tsv[2]))
-                        this_row.append(row_tsv[0])
-                    except IndexError:
-                        logging.debug(this_row)
-                        logging.debug(row_tsv)
-                    aoa.append(this_row)
-        self.run_template('map',
-                          'map_geonames.html',
-                          TABLE_ROWS=json.dumps(aoa),
-                          TABLE_COLUMNS=json.dumps(headers),
-                          TITLE=analysis)
+        ret_tables['total'] = []
+        for series in total_series.itervalues(): ret_tables['total'].append(series.to_dict())
+        for dataset in ret_tables.keys():
+            fname = self.output_dir + 'average_' + analysis + '_by_' + key_group.replace("?", "") + "_" + dataset + ".rq.tsv"
+            with open(fname, "wb") as csvout:
+                writer = csv.DictWriter(csvout, "?key ?size ?min ?max ?amean ?gmean ?hmean".split(" "), delimiter="\t")
+                writer.writeheader()
+                for row in ret_tables[dataset]:
+                    writer.writerow(row)
+            print(fname)
 
 
 if __name__ == '__main__':
     tb = TableBuilder(
         output_dir='out/',
         template_dir='tpl/',
-        analyses_dir='/data/analyses/analysis_2014-04-10_18/',
+        analyses_dir='analysis_2014-04-10_18/',
     )
     dataset_to_country = {
         'bbawdta': 'Germany',
@@ -212,7 +183,9 @@ if __name__ == '__main__':
         'uibwab': 'Norway',
     }
     # tb.write_per_dataset_stats()
-    x = tb.calculate_average('statements-per-resource-and-type', '?type', '?no')
+    # x = tb.calculate_average('statements-per-resource-and-type', '?type', '?no')
+    x = tb.calculate_average('ranges-per-property', '?range', '?no')
+    # x = tb.calculate_average('statements-per-resource-and-type', '?s', '?no')
     # print(json.dumps(x))
     print(x)
     # vis.collate_no('hostnames')
