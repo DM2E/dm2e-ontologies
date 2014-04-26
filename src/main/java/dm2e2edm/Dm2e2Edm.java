182a35b68dddeb19c214079b8be255a8a749e1b7
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-04-26 02:33:08 +0200

    hashcode / equals forgotten

--- src/main/java/dm2e2edm/Dm2e2Edm.java
+++ src/main/java/dm2e2edm/Dm2e2Edm.java
@@ -535,6 +535,28 @@ public class Dm2e2Edm {
 			this.subject = subject;
 			this.predicate = predicate;
 		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((predicate == null) ? 0 : predicate.hashCode());
+			result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) return true;
+			if (obj == null) return false;
+			if (getClass() != obj.getClass()) return false;
+			SubjectPredicate other = (SubjectPredicate) obj;
+			if (predicate == null) {
+				if (other.predicate != null) return false;
+			} else if (!predicate.equals(other.predicate)) return false;
+			if (subject == null) {
+				if (other.subject != null) return false;
+			} else if (!subject.equals(other.subject)) return false;
+			return true;
+		}
 		
 	}
 }
