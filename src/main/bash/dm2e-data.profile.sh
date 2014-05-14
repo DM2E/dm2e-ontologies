8e20fb2d2b2588daf78ec865d4f40d6229147c0f
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-05-14 22:07:47 +0200

    Revamped bash script, more options, batch actions, usage

--- src/main/bash/dm2e-data.profile.sh
+++ src/main/bash/dm2e-data.profile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#---------
-# CONFIG
-#---------
-SPARQL_DIR=$(realpath "../resources/sparql-queries")
-PREFIXES="$SPARQL_DIR/prefixes.rq"
-SPARQL_SCRIPT="$SPARQL_DIR/sparql-query.sh"
-DM2E_EDM_JAR=$(realpath "../../../target/dm2e-edm-jar-with-dependencies.jar")
-DEFAULT_IN_DIR="../../../test/dm2e-dump"
-DEFAULT_OUT_DIR="../../../test/dm2e-edm"
+#-----------------------------------------------#
+# Copy this file to your home directory and     #
+# comment out anything that you want to change  #
+#-----------------------------------------------#
+
+# SPARQL_DIR=$(realpath "../resources/sparql-queries")
+# DM2E_EDM_JAR=$(realpath "../../../target/dm2e-edm-jar-with-dependencies.jar")
+# IN_DIR="/data/dm2e2edm/dm2e-dump"
+# OUT_DIR="/data/dm2e2edm/dm2e-edm"
+# CLEAN_DIR="/data/dm2e2edm/dm2e-edm-clean"
+# DATASET_LIST="dataset.lst"
--- src/main/bash/dm2e-data.sh
+++ src/main/bash/dm2e-data.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
+usage() {
+    if [[ ! -z "$1" ]];then
+        echo $1
+    fi
+    echo "Usage (see README for full docs)
+    
+    bash $0 <action> [options]
+
+    Global options:
+        --purge             Empty directories and list files before writing to them
+        --in-dir            Set \$IN_DIR
+        --out-dir           Set \$IN_DIR
+        --clean-dir         Set \$CLEAN_DIR
+
+    Actions / options:
+        list-datasets       List the latest versions of all datasets
+            --dataset-list              File to write the dataset URIs to [OPTIONAL]
+        list-aggregations   List the latest versions of all datasets
+            --dataset/ds <URI>          URI of the dataset [REQUIRED]
+        dump-aggregations   Dump the transitive closure of the list of aggregations to \$IN_DIR
+            --dataset/ds <URI>          URI of the dataset [REQUIRED]
+        convert-to-edm      Convert the data in \$IN_DIR to EDM RDF/XML in \$OUT_DIR
+        cleanup-edm         Transform the EDM RDF/XML in \$OUT_DIR to prettier RDF/XML in \$CLEAN_DIR
+        batch-dump          Combines 'list-datasets', 'list-aggregations' and 'dump-aggregations'
+        batch-convert       Combines 'convert-to-edm' and 'cleanup-edm'
+"
+    exit 1
+}
+
 HOME_PROFILE="$HOME/dm2e-data.profile.sh"
 LOCAL_PROFILE="./dm2e-data.profile.sh"
+DEFAULT_IN_DIR="./dm2e-dump"
+DEFAULT_OUT_DIR="./dm2e-edm"
+DEFAULT_CLEAN_DIR="./dm2e-edm-clean"
+DEFAULT_DATASET_LIST="./dataset.lst"
 
 #---------
 # CONFIG
 # can be overriden with $HOME/dm2e-data.profile.sh or ./dm2e-data.profile.sh
 #---------
 SPARQL_DIR=$(realpath "../resources/sparql-queries")
-PREFIXES="$SPARQL_DIR/prefixes.rq"
-SPARQL_SCRIPT="$SPARQL_DIR/sparql-query.sh"
 DM2E_EDM_JAR=$(realpath "../../../target/dm2e-edm-jar-with-dependencies.jar")
-DEFAULT_IN_DIR="./dm2e-dump"
-DEFAULT_OUT_DIR="./dm2e-edm"
+IN_DIR="$DEFAULT_IN_DIR"
+OUT_DIR="$DEFAULT_OUT_DIR"
+CLEAN_DIR="$DEFAULT_CLEAN_DIR"
+DATASET_LIST="$DEFAULT_DATASET_LIST"
 
 if [[ -e  $HOME_PROFILE ]];then
     source $HOME_PROFILE
@@ -24,11 +57,12 @@ fi
 #------------
 # PARAMETERS
 #-----------
-FORCE=""
+PREFIXES="$SPARQL_DIR/prefixes.rq"
+SPARQL_SCRIPT="$SPARQL_DIR/sparql-query.sh"
+XSLT_SORT="$SPARQL_DIR/sort.xsl"
 DATASET=""
 IN_FORMAT="RDF/XML"
-IN_DIR="$DEFAULT_IN_DIR"
-OUT_DIR="$DEFAULT_OUT_DIR"
+PURGE=""
 
 clean_uri() {
     local str=$1
@@ -47,18 +81,14 @@ strip_brackets() {
 
 ensure_DATASET() {
     if [[ -z "$DATASET" ]];then
-        echo "Must set dataset '--dataset'/'--ds'"
-        exit 1
+        usage "Must set dataset '--dataset'/'--ds'"
     fi
 }
 
 ensure_AGGREGATIONS_LIST() {
     ensure_DATASET
-    if [[ -z "$AGGREGATIONS_LIST" ]];then
-        AGGREGATIONS_LIST="$(clean_uri $DATASET)-aggregations.lst"
-        touch $AGGREGATIONS_LIST
-    fi
-
+    AGGREGATIONS_LIST="$(clean_uri $DATASET)-aggregations.lst"
+    touch $AGGREGATIONS_LIST
 }
 
 ensure_IN_DIR() {
@@ -73,6 +103,13 @@ ensure_OUT_DIR() {
     fi
 }
 
+ensure_CLEAN_DIR() {
+    ensure_OUT_DIR
+    if [[ ! -e "$CLEAN_DIR" ]];then
+        mkdir -p "$CLEAN_DIR"
+    fi
+}
+
 execute_sparql() {
     local query=$1
     shift
@@ -80,20 +117,27 @@ execute_sparql() {
 }
 
 action_list_datasets() {
-    execute_sparql "SELECT-list-latest-versioned-datasets"
+    result=$(execute_sparql "SELECT-list-latest-versioned-datasets" 2>/dev/null|sed '1d')
+    echo "" > $DATASET_LIST
+    for i in $result;do
+        i=$(strip_brackets $i)
+        echo $i >> $DATASET_LIST
+    done
+    echo "Written list of datasets to '$DATASET_LIST'"
 }
 
 action_list_aggregations() {
     ensure_DATASET
     ensure_AGGREGATIONS_LIST
-    if [[ -s $AGGREGATIONS_LIST && -z "$FORCE" ]];then
-        echo "$AGGREGATIONS_LIST exists and is not empty. Delete and run again to recreate or use --force"
-        exit 1
+    if [[ -s $AGGREGATIONS_LIST && -z "$PURGE" ]];then
+        usage "$AGGREGATIONS_LIST exists and is not empty. Delete and run again to recreate or use --purge"
     fi
-    result=$(execute_sparql "SELECT-list-aggregations-in-dataset" --bind "g" "<$DATASET>" |sed '1d')
-    for i in $result;do
-        echo "$(strip_brackets $i)" >> $AGGREGATIONS_LIST
-    done
+    echo "" > $AGGREGATIONS_LIST
+    echo "Getting all aggregations in <$DATASET>"
+    execute_sparql "SELECT-list-aggregations-in-dataset" --bind "g" "<$DATASET>" 2>/dev/null|sed '1d' > $AGGREGATIONS_LIST
+    echo "Got them. Cleanup."
+    sed -i 's/^<//' $AGGREGATIONS_LIST
+    sed -i 's/>$//' $AGGREGATIONS_LIST
     echo "List of aggregations written to $AGGREGATIONS_LIST"
 }
 
@@ -102,14 +146,17 @@ action_dump_aggregations() {
     ensure_AGGREGATIONS_LIST
     ensure_IN_DIR
     if [[ ! -s $AGGREGATIONS_LIST ]];then
-        echo "$AGGREGATIONS_LIST exists but is empty."
-        exit 1
+        usage "$AGGREGATIONS_LIST exists but is empty."
     fi
+    local total=$(wc -l $AGGREGATIONS_LIST)
+    local cur=0
     for i in $(cat $AGGREGATIONS_LIST);do
         out_filename="$IN_DIR/$(clean_uri $i).xml"
-        execute_sparql "CONSTRUCT-transitivie-closure" --bind 'agg' "<$i>" --bind 'g' "<$DATASET>" --format "xml" > $out_filename
-        echo "Written $i to $out_filename"
+        execute_sparql "CONSTRUCT-transitivie-closure" --bind 'agg' "<$i>" --bind 'g' "<$DATASET>" --format "xml" 2>/dev/null > $out_filename
+        cur=$(( $cur + 1 ))
+        echo -ne "\r[$cur / $total] Dumped <$i> to $out_filename          "
     done
+    echo
 }
 
 action_convert_to_edm() {
@@ -119,6 +166,52 @@ action_convert_to_edm() {
     java -jar $DM2E_EDM_JAR --input_format "RDF/XML" --input_dir $IN_DIR --output_dir $OUT_DIR
 }
 
+action_cleanup_edm() {
+    ensure_CLEAN_DIR
+    for i in $OUT_DIR/*.xml;do
+
+        out_filename="$CLEAN_DIR/$(basename $i)"
+        echo "Cleanup $i -> $out_filename"
+
+        # create tempfiles
+        tmpfile=$(mktemp)
+
+        # produce the right kind of RDF/XML-ABBREV for Europeana's ingestion needs
+        rapper -q -i rdfxml -o rdfxml-abbrev $i > $tmpfile
+
+        # sort the thing with a simple XSLT script
+        xsltproc $XSLT_SORT $tmpfile > $out_filename
+
+        # remove temp files
+        rm $tmpfile
+    done
+}
+
+action_batch_dump() {
+    action_list_datasets
+    for ds in $(cat $DATASET_LIST);do
+        DATASET=$ds
+        ensure_AGGREGATIONS_LIST
+        action_list_aggregations
+        action_dump_aggregations
+    done
+}
+action_batch_convert() {
+    TODO
+}
+
+purge_if_necessary() {
+    dir=$1
+    if [[ ! -z $(ls $dir) && ! -z "$PURGE" ]];then
+        if [[ -d $1 ]];then
+            echo "Are you sure you want to delete all files in '$dir' <yes/no>?"
+            read yesno
+            if [[ "$yesno" == "yes" ]];then
+                rm -v $dir/*
+            fi
+        fi
+    fi
+}
 
 ACTION=$1
 shift
@@ -130,27 +223,41 @@ while [[ ! -z "$1" ]];do
     case "$1" in
         "--ds") ;&
         "--dataset")
-            shift;
-            DATASET=$1
-            shift
+            DATASET=$2
+            shift ; shift
             ;;
-        "--aggregations-list")
-            shift
-            AGGREGATIONS_LIST=$1
-            shift
+        "--dataset-list")
+            DATASET_LIST=$2
+            shift ; shift
             ;;
         "--inFormat")
-            shift
-            IN_FORMAT=$1
+            IN_FORMAT=$2
+            shift ; shift
+            ;;
+        "--in-dir")
+            IN_DIR=$2
+            shift ; shift;
+            ;;
+        "--out-dir")
+            OUT_DIR=$2
+            shift ; shift;
+            ;;
+        "--clean-dir")
+            CLEAN_DIR=$2
+            shift ; shift;
+            ;;
+        "--purge")
+            PURGE="true"
             shift
             ;;
-        "--force")
-            FORCE="true"
-            shift
+        "-h") ;&
+        "--h") ;&
+        "-help") ;&
+        "--help")
+            usage
             ;;
         *)
-            echo "Unknown option '$1'!"
-            exit 1
+            usage "Unknown option '$1'!"
             ;;
     esac
 done
@@ -166,16 +273,30 @@ case "$ACTION" in
         action_list_aggregations
         ;;
     "dump-aggregations")
+        purge_if_necessary $IN_DIR
         action_dump_aggregations
         ;;
     "convert-to-edm")
+        purge_if_necessary $OUT_DIR
         action_convert_to_edm
         ;;
-    "test")
-        clean_uri 'http://data.dm2e.eu/data/dataset/uib/wab/20140311101616905'
+    "cleanup-edm")
+        purge_if_necessary $CLEAN_DIR
+        action_cleanup_edm
+        ;;
+    "batch-dump")
+        purge_if_necessary $IN_DIR
+        action_batch_dump
+        ;;
+    "batch-convert")
+        purge_if_necessary $OUT_DIR
+        purge_if_necessary $CLEAN_DIR
+        action_batch_convert
+        ;;
+    "help")
+        usage
         ;;
     *)
-        echo "Unknown action '$1'!"
-        exit 1
+        usage "Unknown action '$ACTION'!"
         ;;
 esac
